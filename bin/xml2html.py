f5d06f1de054fcbcce1940af753bd78090f88bb9
Maksim Tamkovič <quendimax@gmail.com>
2012-12-26 11:21:27 +0300

    Modify the KamputermSaxHandler class
    * Add the reset method for clearing of tag values
    * Remove \n between tags
    * Commente the style block code

--- bin/xml2html.py
+++ bin/xml2html.py
@@ -9,22 +9,26 @@ class KamputermSaxHandler(xml.sax.handler.ContentHandler):
     def __init__(self):
         xml.sax.ContentHandler.__init__(self)
         self.text = ''
-        self.keys = []
-        self.synonyms = []
-        self.beDefinition = ''
-        self.byDefinition = ''
-        self.mtDefinition = ''
-        self.rmDefinition = ''
+        self.reset()
         self.kind = ''
         self.out = open('dictionary.html', 'w', encoding='utf8')
+
+    def reset(self):
+        self.keys = []
+        self.synonyms = []
+        self.beDefinition = '−'
+        self.byDefinition = '−'
+        self.mtDefinition = '−'
+        self.rmDefinition = '−'
 
     def startDocument(self):
         self.out.write('<html><head>')
         self.out.write('<meta http-equiv="Content-Type" content="text/html;charset=utf8" />')
-        self.out.write('<style>')
-        for line in open('../css/article-style.css'):
-            self.out.write(line)
-        self.out.write('</style></head><body><table border="1" cellspacing="0" cellpadding="4">')
+        #self.out.write('<style>')
+        #for line in open('../css/article-style.css'):
+        #    self.out.write(line)
+        #self.out.write('</style>')
+        self.out.write('</head><body><table border="1" cellspacing="0" cellpadding="4">')
         self.out.write('<tr><th>Тэрмін</th><th>Сынонім</th><th>Пераклад (школьны правапіс)</th>')
         self.out.write('<th>Пераклад (класічны правапіс)</th><th>Сустрэчы</th><th>Каметар</th>')
 
@@ -34,8 +38,11 @@ class KamputermSaxHandler(xml.sax.handler.ContentHandler):
     def startElement(self, name, attrs):
         if name != 'definition':
             return
-        self.kind = attrs.getValue('kind')
-        
+        if 'kind' in attrs.getNames():
+            self.kind = attrs.getValue('kind')
+        else:
+            self.kind = 'al'
+
     def endElement(self, name):
         if name == 'article':
             #TODO start
@@ -49,25 +56,18 @@ class KamputermSaxHandler(xml.sax.handler.ContentHandler):
                     self.out.write(synonym)
                     self.out.write('<br>')
             else:
-                self.out.write('-')
-            self.out.write('</td>\n<td>')
+                self.out.write('−')
+            self.out.write('</td><td>')
             self.out.write(self.byDefinition)
-            self.out.write('</td>\n<td>')
+            self.out.write('</td><td>')
             self.out.write(self.beDefinition)
-            self.out.write('</td>\n<td>')
+            self.out.write('</td><td>')
             self.out.write(self.mtDefinition)
-            self.out.write('</td>\n<td>')
+            self.out.write('</td><td>')
             self.out.write(self.rmDefinition)
-            self.out.write('</tr>\n')
+            self.out.write('</tr>')
             #TODO end
-            self.key = ''
-            self.keys = []
-            self.synonym = ''
-            self.synonyms = []
-            self.beDefinition = ''
-            self.byDefinition = ''
-            self.mtDefinition = ''
-            self.rmDefinition = ''
+            self.reset()
         elif name == 'key':
             self.keys.append(self.text.strip(' \n\t'))
         elif name == 'synonym':
